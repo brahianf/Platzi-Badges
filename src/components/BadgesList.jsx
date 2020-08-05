f04468dc4692ae931a09fafaf964758b5de6e4e8
brahianf <brahianf@unicauca.edu.co>
2020-08-05 00:46:02 -0500

    Custom Hooks Search filter

--- src/components/BadgesList.jsx
+++ src/components/BadgesList.jsx
@@ -5,57 +5,94 @@ import './styles/BadgesList.css'
 import Gravatar from './Gravatar.jsx'
 
 class BadgesListItem extends React.Component {
-    render() {
-      return (
-        <div className="BadgesListItem">
-          <Gravatar
-              className="badge_avatar"
-              email = {this.props.badge.email}
-              alt="Avatar"
-          />
-  
-          <div>
-            <strong>
-              {this.props.badge.firstName} {this.props.badge.lastName}
-            </strong>
-            <br />@{this.props.badge.twitter}
-            <br />
-            {this.props.badge.jobTitle}
-          </div>
-        </div>
-      );
-    }
-  }
+  render() {
+    return (
+      <div className="BadgesListItem">
+        <Gravatar
+            className="badge_avatar"
+            email = {this.props.badge.email}
+            alt="Avatar"
+        />
 
-class BadgesList extends React.Component {
-    render() {
-      if(this.props.badges.length == 0) {
-        return (
-          <div>
-            <h3>No badges were found</h3>
-            <Link className="btn btn-primary" to="/badges/new">
-              Create new badge
-            </Link>
-          </div>
-        )
-      }
-        return (
-            <div className="BadgesList">
-                <ul className="list-unstyled">
-                    {this.props.badges.map((badge) =>{
-                        return (
-                            <li key={badge.id}>
-                              <Link to={`/badges/${badge.id}`} className="text-reset text-decoration-none">
-                                {/* <p>{badge.firstName} {badge.lastName}</p> */}
-                                <BadgesListItem badge={badge} />
-                              </Link>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-        )
-    }
+        <div>
+          <strong>
+            {this.props.badge.firstName} {this.props.badge.lastName}
+          </strong>
+          <br />@{this.props.badge.twitter}
+          <br />
+          {this.props.badge.jobTitle}
+        </div>
+      </div>
+    );
+  }
+}
+
+function useSearchBadges (badges) {
+  const [ query , setQuery] = React.useState('')
+  const [filteredBadges, setFilteredBadges] = React.useState(badges)
+
+  React.useMemo(() => {
+      const result = badges.filter( badge => {
+        return `${badge.firstName} ${badge.lastName}`
+        .toLowerCase()
+        .includes(query.toLowerCase());
+      });
+
+      setFilteredBadges(result)
+  }, [badges, query]);
+  return { query, setQuery, filteredBadges };
+}
+
+function BadgesList(props) {
+  const badges = props.badges;
+  const { query, setQuery, filteredBadges } = useSearchBadges(badges);
+
+  if(filteredBadges.length == 0) {
+    return (
+      <div>
+      <div className="form-group">
+        <label>Filter Badges</label>
+        <input type="text" className="form-control"
+          value={query}
+          onChange={(e) => {
+          setQuery(e.target.value)
+          }}
+        />
+      </div>
+
+        <h3>No badges were found</h3>
+        <Link className="btn btn-primary" to="/badges/new">
+          Create new badge
+        </Link>
+      </div>
+    )
+  }
+  return (
+    <div className="BadgesList">
+      <div className="form-group">
+        <label>Filter Badges</label>
+        <input type="text" className="form-control"
+          value={query}
+          onChange={(e) => {
+            setQuery(e.target.value)
+          }}
+        />
+      </div>
+
+        <ul className="list-unstyled">
+            {filteredBadges.map((badge) =>{
+                return (
+                    <li key={badge.id}>
+                      <Link to={`/badges/${badge.id}`} className="text-reset text-decoration-none">
+                        {/* <p>{badge.firstName} {badge.lastName}</p> */}
+                        <BadgesListItem badge={badge} />
+                      </Link>
+                    </li>
+                );
+            })}
+        </ul>
+    </div>
+  )
 }
 
 export default BadgesList;
